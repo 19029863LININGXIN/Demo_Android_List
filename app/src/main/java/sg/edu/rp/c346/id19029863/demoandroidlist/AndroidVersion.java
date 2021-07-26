a3814a25d09d7124e0b3505bc089aa36b9bc9a47
19029863 <19029863@myrp.edu.sg>
2021-07-26 14:08:28 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19029863/demoandroidlist/AndroidVersion.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id19029863.demoandroidlist;
+
+public class AndroidVersion {
+
+    private String version;
+    private String name;
+
+    public AndroidVersion(String name, String version) {
+        this.version = version;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + version;
+    }
+}
